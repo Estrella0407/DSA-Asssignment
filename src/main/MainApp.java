358f80a2fb93de2ac7fdcfee8090e36f2ba0d311
Tsu123559 <qinqilawshit@gmail.com>
2026-08-10 16:10:03 +0800

    Import MainMenuUI and update main method
    Added MainMenuUI import and updated main method to display the main menu.

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -1,5 +1,17 @@
 package main;
 
+import boundary.MainMenuUI;
+
+/**
+ * Main entry point for the TARUMT Resorts Application.
+ * 
+ * @author -
+ */
+
 public class MainApp {
-    
+
+    public static void main(String[] args) {
+        MainMenuUI menu = new MainMenuUI();
+        menu.displayMainMenu();
+    }
 }
